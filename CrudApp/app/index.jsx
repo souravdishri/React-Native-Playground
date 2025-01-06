fa18719288e415e1b67e953853a07edda66be4c6
Sourav Dishri <souravdishri@gmail.com>
2025-01-06 19:50:56 +0530

    Async Storage

--- CrudApp/app/index.jsx
+++ CrudApp/app/index.jsx
@@ -1,22 +1,58 @@
 import { Text, View, TextInput, Pressable, StyleSheet, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { ThemeContext } from "@/context/ThemeContext";
-
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
-import { data } from "@/data/todo"
 import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
-import Octicons from "@expo/vector-icons/Octicons";
+import Animated, { LinearTransition } from 'react-native-reanimated'
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StatusBar } from "expo-status-bar";  //show status bar
+
+import Octicons from '@expo/vector-icons/Octicons'
+
+import { data } from "@/data/todo"
 
 export default function Index() {
-  const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id))
-  const [text, setText] = useState('')    //for input
+  const [todos, setTodos] = useState([])
+  const [text, setText] = useState('')
   const { colorScheme, setColorScheme, theme } = useContext(ThemeContext)
 
   const [loaded, error] = useFonts({
     Inter_500Medium,
-  }) 
+  })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem("TodoApp")
+        const storageTodos = jsonValue != null ? JSON.parse(jsonValue) : null
+
+        if (storageTodos && storageTodos.length) {
+          setTodos(storageTodos.sort((a, b) => b.id - a.id))
+        } else {
+          setTodos(data.sort((a, b) => b.id - a.id))
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    fetchData()
+  }, [data])
+
+  useEffect(() => {
+    const storeData = async () => {
+      try {
+        const jsonValue = JSON.stringify(todos)
+        await AsyncStorage.setItem("TodoApp", jsonValue)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    storeData()
+  }, [todos])
 
   if (!loaded && !error) {
     return null
@@ -29,7 +65,7 @@ export default function Index() {
   //Create
   const addTodo = () => {
     if (text.trim()) {
-      const newId = todos.length > 0 ? todos[0].id + 1 : 1;   //because we are sorting in descending order
+      const newId = todos.length > 0 ? todos[0].id + 1 : 1;
       setTodos([{ id: newId, title: text, completed: false }, ...todos])
       setText('')
     }
@@ -37,15 +73,15 @@ export default function Index() {
 
   // The toggleTodo function is used to update the 'completed' status of a to-do item
   //Update (here we are toggling here)
-  const toggleTodo = (id) => {  //updating the 'completed' status of a todo item based on its id
+  const toggleTodo = (id) => {
     setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
 
-    // setTodos(todos.map((todo) => {
-    //   //it creates a new object with the same properties as todo, but with completed set to the opposite value (true becomes false, and false becomes true).
-    //   return todo.id === id ? { ...todo, completed: !todo.completed } : todo;
-    // }));
-    
+  // setTodos(todos.map((todo) => {
+  //   //it creates a new object with the same properties as todo, but with completed set to the opposite value (true becomes false, and false becomes true).
+  //   return todo.id === id ? { ...todo, completed: !todo.completed } : todo;
+  // }));
   }
+
   //Delete
   //(The filter method is used to create a new array that only includes items that do not match the given id.)
   const removeTodo = (id) => {
@@ -55,15 +91,14 @@ export default function Index() {
   const renderItem = ({ item }) => (
     <View style={styles.todoItem}>
       <Text
-        //to add more style properties,(if 'item.completed' is true, we add the 'styles.completedText' to the existing styles.todoText)
+       //to add more style properties,(if 'item.completed' is true, we add the 'styles.completedText' to the existing styles.todoText)
         style={[styles.todoText, item.completed && styles.completedText]}
         onPress={() => toggleTodo(item.id)}
       >
         {item.title}
       </Text>
-      
       <Pressable onPress={() => removeTodo(item.id)}>
-        <MaterialCommunityIcons name="delete-circle" size={35} color="#FF7F3E" selectable={undefined} />
+        <MaterialCommunityIcons name="delete-circle" size={36} color="red" selectable={undefined} />
       </Pressable>
     </View>
   )
@@ -81,21 +116,25 @@ export default function Index() {
         <Pressable onPress={addTodo} style={styles.addButton}>
           <Text style={styles.addButtonText}>Add</Text>
         </Pressable>
-        
+
         <Pressable
           onPress={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')} style={{ marginLeft: 10 }}>
+
           <Octicons name={colorScheme === 'dark' ? "moon" : "sun"} size={36} color={theme.text} selectable={undefined} style={{ width: 36 }} />
+
         </Pressable>
 
       </View>
-
-      <FlatList
+      <Animated.FlatList
         data={todos}
         renderItem={renderItem}
         keyExtractor={todo => todo.id}
         contentContainerStyle={{ flexGrow: 1 }}
+        itemLayoutAnimation={LinearTransition}
+        keyboardDismissMode="on-drag"
       />
-
+      {/* show status bar */}
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
     </SafeAreaView>
   );
 }
